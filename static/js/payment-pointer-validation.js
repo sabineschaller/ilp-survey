abe5c9e1aaf153b4affddd28cc913eee741689ee
Sabine Bertram <sabinebertram@Vernons-MBP-2.localdomain>
2018-12-04 15:56:28 -0800

    form validation

---
+++ static/js/payment-pointer-validation.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    window.addEventListener('load', function () {
+
+        const form = document.querySelector('form');
+        const input = document.getElementById('pp');
+
+        form.addEventListener('submit', function (event) {
+            if (validPointer(input.value) === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            } else {
+                storePointer(input.value);
+            }
+            input.value = '';
+            form.classList.add('was-validated');
+        }, false);
+    });
+
+    function validPointer(pointer) {
+        let startswith$ = pointer.startsWith('$');
+        return startswith$;
+    }
+
+    function storePointer(pointer) {
+        sessionStorage.setItem('pointer', pointer);
+    }
+})();
