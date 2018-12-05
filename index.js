4511e590dcdf57709ea69e91daf146e5f2acacba
Sabine Bertram <sabinebertram@Vernons-MBP-2.localdomain>
2018-12-05 13:15:45 -0800

    refactoring to allow templating

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-const Koa = require('koa');
-const router = require('koa-router')();
-const serve = require('koa-static');
-const bodyParser = require('koa-bodyparser');
 const path = require('path');
+const Koa = require('koa');
+const render = require('koa-ejs');
+const serve = require('koa-static');
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 const demographics = require('./src/demographics');
 const question = require('./src/question');
+
 const app = new Koa();
 
 app.use(serve(path.resolve(__dirname, 'static')));
 app.use(bodyParser());
 
-router.get('/', ctx => {
-    ctx.redirect('/welcome.html');
+render(app, {
+  root: path.join(__dirname, 'static'),
+  layout: false,
+  viewExt: 'html',
+  cache: false,
+  debug: false
+});
+
+router.get('/', async ctx => {
+    await ctx.render('welcome');
     ctx.status = 301;
 });
 
-router.post('/welcome.html', ctx => {
-    ctx.redirect('/demographics.html');
-});
+router.post('/', async ctx => {
 
-router.post('/demographics.html', ctx => {
-    if (demographics.process(ctx.request.body) === true) {
-        ctx.redirect('/instructions.html');
-    } else {
-        ctx.redirect('/welcome.html');
+    if (ctx.request.body['form-origin'] === 'welcome'){
+        await ctx.render('demographics');
+    } 
+    
+    else if (ctx.request.body['form-origin'] === 'demographics') {
+        if (demographics.process(ctx.request.body) === true) {
+            await ctx.render('instructions');
+        } else {
+            await ctx.render('welcome');
+        }
     }
-});
 
-router.post('/question.html', ctx => {
-    if (question.process(ctx.request.body) === true) {
-        ctx.redirect('/thanks.html');
-    } else {
-        ctx.redirect('/welcome.html');
+    if (ctx.request.body['form-origin'] === 'instructions'){
+        await ctx.render('question');
+    } 
+
+    else if (ctx.request.body['form-origin'] === 'question') {
+        if (question.process(ctx.request.body) === true) {
+            await ctx.render('thanks');
+        } else {
+            await ctx.render('welcome');
+        }
     }
 });
 
