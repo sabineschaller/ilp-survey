773c13107ffff219868b6cd1ae8c5b658c61c7ae
Sabine Bertram <sabine.bertram@mailbox.org>
2019-01-07 16:47:31 -0800

    retrieve answers includes pw and id=pp

--- src/answers.js
+++ src/answers.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const redis = require('./redis-functions');
+const helpers = require('./helpers');
 
 async function process(obj) {
     let survey = await redis.getOneSurvey('s' + obj['survey-id']);
-    if (survey.pointer !== obj.pp) {
+    if (survey.password !== helpers.hashCode(obj.pw)) {
         return {};
     } else {
         return await redis.surveys.getAsync('a' + obj['survey-id']);
